0257fa393d68b52d658b5382c54127ccfc1b8273
Ellen Coutinho <ellencls@al.insper.edu.br>
2023-11-20 17:56:53 -0300

    Token sendo exibido no frontend

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,6 +11,7 @@ export default function Login() {
     const [userExists, setUserExists] = useState(false);
     const [currentScreen, setCurrentScreen] = useState("Login");
     const [postOk, setPostOk] = useState(true);
+    const [hasToken, setHasToken] = useState(false);
 
     function SignUP() {
         if (!postOk) {
@@ -21,9 +22,11 @@ export default function Login() {
     const login = (event) => {
         event.preventDefault();
         if(userExists){
-            setUrlUser("http://localhost:8000/api/token/")
+            setUrlUser("http://localhost:8000/api/token/");
+            setHasToken(true);
         } else {
-            setUrlUser("http://localhost:8000/api/users/")
+            setUrlUser("http://localhost:8000/api/users/");
+            setUserExists(true);
         }
 
         const userInfo = {
@@ -34,31 +37,39 @@ export default function Login() {
         
         axios
             .post(urlUser, userInfo)
-            .then(() => {setPostOk(true); setUserExists(true)})
+            .then((response) => {
+                setPostOk(true); 
+                setUserExists(true);
+                if(hasToken){
+                    const token = response.data.token;
+                    console.log(token);
+                }}
+                )
             .catch((error) => setPostOk(false));
         };
 
     return (
         <div className="content">
             {currentScreen==="Login" ? (
-                <><h4>Login</h4><form onSubmit={login}>
-                <div className="form-group row">
-                    <div className="col-md-2">
-                        <label>User</label>
+                <><h4>Login</h4>
+                <form onSubmit={login}>
+                    <div className="form-group row">
+                        <div className="col-md-2">
+                            <label>User</label>
+                        </div>
+                        <div className="col-md-9">
+                            <input type="text" value={username} className="form-control" onChange={(event) => setUsername(event.target.value)} />
+                        </div>
                     </div>
-                    <div className="col-md-9">
-                        <input type="text" value={username} className="form-control" onChange={(event) => setUsername(event.target.value)} />
+                    <div className="form-group row">
+                        <div className="col-md-2">
+                            <label>Password</label>
+                        </div>
+                        <div className="col-md-9">
+                            <input type="password" value={password} className="form-control" onChange={(event) => setPassword(event.target.value)} />
+                        </div>
                     </div>
-                </div>
-                <div className="form-group row">
-                    <div className="col-md-2">
-                        <label>Password</label>
-                    </div>
-                    <div className="col-md-9">
-                        <input type="password" value={password} className="form-control" onChange={(event) => setPassword(event.target.value)} />
-                    </div>
-                </div>
-                <input type="submit" value="Login" />
+                    <input type="submit" value="Login" />
             </form><button onClick={() => setCurrentScreen("Cadastro")  }>Ainda não é cadastrado? clique aqui!</button>
             <SignUP/>
             </>
@@ -91,7 +102,7 @@ export default function Login() {
                         </div>
                     </div>
                     <button onClick={login}>Cadastrar</button>
-                </form><button onClick={() => {setCurrentScreen("Login"); setUserExists(true)}}>Fazer login</button></>  
+                </form><button onClick={() => {setCurrentScreen("Login")}}>Fazer login</button></>  
                 )}
         </div>
     )
