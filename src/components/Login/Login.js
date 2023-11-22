605d54f8f8dd0e9a9d4d25e78b7c94dc01d1c464
kadu-ymd <carlosepy@al.insper.edu.br>
2023-11-22 19:02:08 -0300

    Estilização tela de Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import "./Login.css"
@@ -11,23 +11,27 @@ export default function Login() {
     const [password, setPassword] = useState("");
     const [currentScreen, setCurrentScreen] = useState("Login");
     const [postOk, setPostOk] = useState(true);
-    const [hasToken, setHasToken] = useState(false);
+    const [signedUp, setSignedUp] = useState(false);
     const navigate = useNavigate();
 
-
-    function SignUP() {
+    function SignUp() {
         if (!postOk) {
-          return <p>Você ainda não está cadastrado!</p>;
+            return <p className="sign-up-text">Usuário e/ou senha não encontrados!</p>;
         }
-      }
+    }
+
+    function SignedUp() {
+        if (signedUp) {
+            return <p className="signed-up-text">Usuário cadastrado com sucesso!</p>
+        }
+    }
 
     const login = (event) => {
         event.preventDefault();
-        console.log('currentScreen', currentScreen);
+        console.log("currentScreen", currentScreen);
 
-        if(currentScreen === 'Login'){
+        if (currentScreen === "Login") {
             urlUser = "http://localhost:8000/api/token/";
-            setHasToken(true);
         } else {
             urlUser = "http://localhost:8000/api/users/";
         }
@@ -36,80 +40,121 @@ export default function Login() {
             "username": username,
             "email": email,
             "password": password
-          };
-        
-        console.log('Url user', urlUser);
+        };
+
+        console.log("Url user", urlUser);
 
         axios
             .post(urlUser, userInfo)
             .then((response) => {
-                setPostOk(true); 
+                setPostOk(true);
                 if (currentScreen === "Login") {
                     const token = response.data.token;
-                    localStorage.setItem('token', true);
+                    localStorage.setItem("token", true);
                     console.log(token);
-                    navigate('/chat');
+                    navigate("/chat");
+                } else {
+                    console.log("cadastrado com sucesso")
+                    setSignedUp(true);
+                    setCurrentScreen("Cadastro");
                 }
             }).catch((error) => setPostOk(false));
-        };
+    };
 
     return (
         <div className="content">
-            {currentScreen==="Login" ? (
-                <><h4>Login</h4>
-                <form onSubmit={login}>
-                    <div className="form-group row">
-                        <div className="col-md-2">
-                            <label>User</label>
+            {currentScreen === "Login" ? (
+                <div className="box-login">
+                    <h3>Login</h3>
+                    <form className="box-form-login" onSubmit={login}>
+                        <div className="box-input">
+                            <label for="user">
+                                Usuário
+                            </label>
+                            <input
+                                className="input-text"
+                                type="text"
+                                id="user"
+                                value={username}
+                                onChange={(event) => setUsername(event.target.value)}
+                                required
+                            />
                         </div>
-                        <div className="col-md-9">
-                            <input type="text" value={username} className="form-control" onChange={(event) => setUsername(event.target.value)} />
+                        <div className="box-input">
+                            <label for="password">
+                                Senha
+                            </label>
+                            <input
+                                className="input-text"
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(event) => setPassword(event.target.value)}
+                                required
+                            />
                         </div>
-                    </div>
-                    <div className="form-group row">
-                        <div className="col-md-2">
-                            <label>Password</label>
+                        <input
+                            className="login-button"
+                            type="submit"
+                            value="Login"
+                        />
+                    </form>
+                    <span>
+                        Ainda não é cadastrado?
+                        <button className="redirect" onClick={() => setCurrentScreen("Cadastro")}>Clique aqui!</button>
+                    </span>
+                    <SignUp />
+                </div>
+            ) : (
+                <div className="box-signup">
+                    <h3>Cadastro</h3>
+                    <form className="box-form-signup" onSubmit={login}>
+                        <div className="box-input">
+                            <label for="user">Usuário</label>
+                            <input
+                                className="input-text"
+                                id="user"
+                                type="text"
+                                value={username}
+                                onChange={(event) => setUsername(event.target.value)}
+                                required
+                            />
                         </div>
-                        <div className="col-md-9">
-                            <input type="password" value={password} className="form-control" onChange={(event) => setPassword(event.target.value)} />
+                        <div className="box-input">
+                            <label for="email">E-mail</label>
+                            <input
+                                className="input-text"
+                                id="email"
+                                type="email"
+                                value={email}
+                                onChange={(event) => setEmail(event.target.value)}
+                                required
+                            />
                         </div>
-                    </div>
-                    <input type="submit" value="Login" />
-            </form><button onClick={() => setCurrentScreen("Cadastro")  }>Ainda não é cadastrado? clique aqui!</button>
-            <SignUP/>
-            </>
-                
-                ) : (
-                    <><h4>Cadastro</h4>
-                    <form>
-                    <div className="form-group row">
-                        <div className="col-md-2">
-                            <label>User</label>
+                        <div className="box-input">
+                            <label for="password">Senha</label>
+                            <input
+                                type="password"
+                                id="password"
+                                value={password}
+                                className="input-text"
+                                onChange={(event) => setPassword(event.target.value)}
+                                required
+                            />
                         </div>
-                        <div className="col-md-9">
-                            <input type="text" value={username} className="form-control" onChange={(event) => setUsername(event.target.value)} />
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <div className="col-md-2">
-                            <label>Email</label>
-                        </div>
-                        <div className="col-md-9">
-                            <input type="text" value={email} className="form-control" onChange={(event) => setEmail(event.target.value)} />
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <div className="col-md-2">
-                            <label>Password</label>
-                        </div>
-                        <div className="col-md-9">
-                            <input type="password" value={password} className="form-control" onChange={(event) => setPassword(event.target.value)} />
-                        </div>
-                    </div>
-                    <button onClick={login}>Cadastrar</button>
-                </form>
-                <button onClick={() => {setCurrentScreen("Login")}}>Fazer login</button></>  
-                )}
+                        <input
+                            className="signup-button"
+                            type="submit"
+                            value="Cadastrar"
+                        />
+                    </form>
+                    <span>
+                        Já tem uma conta?
+                        <button className="redirect" onClick={() => { setCurrentScreen("Login") }}>Faça login!</button>
+                    </span>
+                    <SignedUp />
+                </div>
+            )}
         </div>
     )
 }
